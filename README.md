99916d707b083039dbd2a2c9b3495c0c66bfdb95
Willem Sonke <willem@wimiso.nl>
2020-07-09 20:21:37 +0200

    Use the Reykunyu API for searching

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 This is a (currently very incomplete!) parser for the Na'vi language.
 
-It uses navi-reykunyu to parse individual words in the input sentence, and then uses a nearley grammar to parse these tokens into a parse tree.
+It uses Reykunyu's API (at https://reykunyu.wimiso.nl) to parse individual words in the input sentence, and then uses a nearley grammar to parse these tokens into a parse tree.
 
 ## Usage
 
 * Install [nearley](https://github.com/kach/nearley): `sudo npm install -g nearley`
 * Compile the grammar: `nearleyc navi.ne -o navi.js`
-* Install [navi-reykunyu](https://github.com/Willem3141/navi-reykunyu) by cloning that repository to the same directory where you cloned navi-tslamyu
-* Install the database of Na'vi words [navi-tsim](https://github.com/Willem3141/navi-tsim) by cloning that somewhere, and linking (or copying) the directory `aylì'u` to the `navi-reykunyu` directory.
 * Parse a sentence: `node parser.js "<sentence to parse>"`
-
-(Yes, this procedure is ugly. It will be made more streamlined in the future...)
 
 ## Supported grammar features
 
--- parser.js
+++ parser.js
@@ -1,100 +1,124 @@
 const nearley = require('nearley');
 const grammar = require('./navi.js');
 const util = require('util');
-const reykunyu = require('../navi-reykunyu/searcher.js');
+
+const fetch = require('node-fetch');
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
-let input = process.argv[2];
-let responses = reykunyu.getResponsesFor(input);
-let tokens = [];
-for (let i = 0; i < responses.length; i++) {
-	let response = responses[i];
-	let token = {};
-	token['value'] = response['tìpawm'];
-	let types = [];
-	for (let j = 0; j < response['sì\'eyng'].length; j++) {
-		let type = getGrammarTypeOf(response['sì\'eyng'][j]);
-		if (typeof type === "string") {
-			types.push(type);
-		} else {
-			types = types.concat(type);
+main();
+
+async function main() {
+	let input = process.argv[2];
+	let responses = await getResponsesFor(input);
+	let tokens = [];
+	for (let i = 0; i < responses.length; i++) {
+		let response = responses[i];
+		let token = {};
+		token['value'] = response['tìpawm'];
+		let types = [];
+		for (let j = 0; j < response['sì\'eyng'].length; j++) {
+			let type = getGrammarTypeOf(response['sì\'eyng'][j]);
+			if (typeof type === "string") {
+				types.push(type);
+			} else if (type) {
+				types = types.concat(type);
+			}
 		}
+		token['types'] = types;
+		token['definition'] = response['sì\'eyng'];
+		tokens.push(token);
 	}
-	token['types'] = types;
-	token['definition'] = response['sì\'eyng'];
-	tokens.push(token);
+
+	console.log('\x1b[1m\x1b[34mInput:\x1b[0m');
+	for (let i = 0; i < tokens.length; i++) {
+		let token = tokens[i];
+		console.log('\x1b[1m' + token['value'] + '\x1b[0m (' + token['types'].join(', ') + ')');
+	}
+
+	/*let inputTokens = [
+		{'value': 'nìngay', 'types': ['adv']},
+		{'value': 'rey', 'types': ['vin']},
+		{'value': 'po', 'types': ['n_subjective']},
+		{'value': 'a', 'types': ['a_left', 'a_right']},
+		{'value': 'tute', 'types': ['n_subjective']},
+		{'value': 'apxa', 'types': ['adj', 'adj_left', 'adj_right']},
+		{'value': 'tul', 'types': ['vin']}
+	];*/
+	/*let input = [
+		{'value': 'taronyul', 'types': ['n_agentive']},
+		{'value': 'apxa', 'types': ['adj', 'adj_left', 'adj_right']},
+		{'value': 'ioangit', 'types': ['n_patientive']},
+		{'value': 'taron', 'types': ['vtr']}
+	];*/
+	parser.feed(tokens);
+
+	//console.log(util.inspect(parser.results, false, null, true));
+
+	function getGrammarTypeOf(word) {
+		let t = word['type'];
+		let type = null;
+		if (t === 'n' || t === 'pn') {
+			let suffix = word['conjugated'][2][5];
+			switch (suffix) {
+				case '':
+					type = 'n_subjective'; break;
+				case 'l':
+					type = 'n_agentive'; break;
+				case 't':
+					type = 'n_patientive'; break;
+				case 'r':
+					type = 'n_dative'; break;
+				case 'ä':
+					type = 'n_genitive'; break;
+				case 'ri':
+					type = 'n_topical'; break;
+				default:
+					//throw new Error('unknown suffix: ' + suffix);
+					break;
+			}
+		}
+		if (t === 'v:in') {
+			type = 'vin';
+		}
+		if (t === 'v:tr') {
+			type = ['vin', 'vtr'];
+		}
+		if (t === 'part') {
+			if (word['na\'vi'] === 'a') {
+				type = ['a_left', 'a_right'];
+			} else if (word['na\'vi'] === 'ma') {
+				type = 'ma';
+			}
+		}
+		if (t === 'adv') {
+			type = 'adv';
+		}
+		//if (type === '') {
+		//	throw new Error('tslamyu doesn\'t understand word type: ' + t);
+		//}
+		return type;
+	}
+
+	console.log();
+	console.log('\x1b[1m\x1b[34mParse results:\x1b[0m ' + parser.results.length + ' possible parse tree(s) found');
+	for (let i = 0; i < parser.results.length; i++) {
+		let result = parser.results[i];
+		//console.log(JSON.stringify(result));
+		//console.log(JSON.stringify(verbClauseToTree(result)));
+		outputTree(verbClauseToTree(result));
+	}
 }
 
-console.log('\x1b[1m\x1b[34mInput:\x1b[0m');
-for (let i = 0; i < tokens.length; i++) {
-	let token = tokens[i];
-	console.log('\x1b[1m' + token['value'] + '\x1b[0m (' + token['types'].join(', ') + ')');
-}
+async function getResponsesFor(query) {
+	const response = await fetch('https://reykunyu.wimiso.nl/api/fwew?tìpawm=' + query)
+		.then(response => response.json())
+		.catch(error => {
+			message.channel.send("Something went wrong while searching. Please try again later, or ping Wllìm if this problem persists.")
+			return;
+		});
 
-/*let inputTokens = [
-	{'value': 'nìngay', 'types': ['adv']},
-	{'value': 'rey', 'types': ['vin']},
-	{'value': 'po', 'types': ['n_subjective']},
-	{'value': 'a', 'types': ['a_left', 'a_right']},
-	{'value': 'tute', 'types': ['n_subjective']},
-	{'value': 'apxa', 'types': ['adj', 'adj_left', 'adj_right']},
-	{'value': 'tul', 'types': ['vin']}
-];*/
-/*let input = [
-	{'value': 'taronyul', 'types': ['n_agentive']},
-	{'value': 'apxa', 'types': ['adj', 'adj_left', 'adj_right']},
-	{'value': 'ioangit', 'types': ['n_patientive']},
-	{'value': 'taron', 'types': ['vtr']}
-];*/
-parser.feed(tokens);
-
-//console.log(util.inspect(parser.results, false, null, true));
-
-function getGrammarTypeOf(word) {
-	let type = word['type'];
-	if (type === 'n' || type === 'pn') {
-		let suffix = word['conjugated'][2][5];
-		switch (suffix) {
-			case '':
-				type = 'n_subjective'; break;
-			case 'l':
-				type = 'n_agentive'; break;
-			case 't':
-				type = 'n_patientive'; break;
-			case 'r':
-				type = 'n_dative'; break;
-			case 'ä':
-				type = 'n_genitive'; break;
-			case 'ri':
-				type = 'n_topical'; break;
-			default:
-				throw new Error('unknown suffix: ' + suffix);
-		}
-	}
-	if (type === 'v:in') {
-		type = 'vin';
-	}
-	if (type === 'v:tr') {
-		type = ['vin', 'vtr'];
-	}
-	if (type === 'part') {
-		if (word['na\'vi'] === 'a') {
-			type = ['a_left', 'a_right'];
-		} else if (word['na\'vi'] === 'ma') {
-			type = 'ma';
-		}
-	}
-	return type;
-}
-
-console.log();
-console.log('\x1b[1m\x1b[34mParse results:\x1b[0m ' + parser.results.length + ' possible parse tree(s) found');
-for (let i = 0; i < parser.results.length; i++) {
-	let result = parser.results[i];
-	//console.log(JSON.stringify(result));
-	//console.log(JSON.stringify(verbClauseToTree(result)));
-	outputTree(verbClauseToTree(result));
+	return response;
 }
 
 function wordToString(word) {
